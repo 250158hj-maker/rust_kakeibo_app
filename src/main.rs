c8a025a783dee85f77d73cc067e0fd8b2791fa80
takumi <250158hj@yse-c.net>
2026-05-30 10:25:03 +0900

    feat: 標準入力の受け入れと入力値エラーハンドリング

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rust_kakeibo_app::services;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut service_type = String::new();
+    println!("実行居たい内容を入力してください(0: 登録, 1: 集計)");
+    io::stdin().read_line(&mut service_type).unwrap();
+    let service_type: u8 = service_type.trim().parse().expect("数値で入力してください");
+    services::validate::InputValidator::validate_service_type(service_type);
+
+    if service_type == 0 {
+        println!("toouroku service");
+    } else if service_type == 1 {
+        println!("shukei service");
+    }
 }
